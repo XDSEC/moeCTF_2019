3dd93d1d022f787cb57bb39346069ab64f1910a8
Frank <frankli0324@hotmail.com>
2019-08-11 00:00:06 +0800

    Update solve.py

--- programming/TreeDistance/solve.py
+++ programming/TreeDistance/solve.py
@@ -8,8 +8,3 @@ for i in range(200):
     ret = ses.get('http://judge.qwer.design:12354/api/decrease/'+str(i)).text
     if 'moectf' in ret:
         break
-
-for i in range(100):
-    ret = ses.post('http://judge.qwer.design:12354/', data={
-        'ans': 1
-    })
